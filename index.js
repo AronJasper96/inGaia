929fa12332bb2fff4b0009bbefdfe6712a979408
Aron Jasper Maziero <aron.jasper@gmail.com>
2021-01-27 13:56:11 -0300

    init

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/', indexRouter);
 
-
 app.listen(3000, function () {
   console.log('Escutando na porta 3000!');
 });
